114cf24a7d635fb57f30507792974a584d72784c
Matt Wagantall <mattw@codeaurora.org>
2013-12-05 20:01:32 -0800

    sched/debug: Make sysrq prints of sched debug data optional
    Calls to sysrq_sched_debug_show() can yield rather verbose output
    which contributes to log spew and, under heavy load, may increase
    the chances of a watchdog bark.
    
    Make printing of this data optional with the introduction of a
    new Kconfig, CONFIG_SYSRQ_SCHED_DEBUG.
    
    Change-Id: I5f54d901d0dea403109f7ac33b8881d967a899ed
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5232,7 +5232,7 @@ void show_state_thread_filter(unsigned long state_filter,
 
 	touch_all_softlockup_watchdogs();
 
-#ifdef CONFIG_SCHED_DEBUG
+#ifdef CONFIG_SYSRQ_SCHED_DEBUG
 	if (threads_filter & SHOW_KTHREADS)
 		sysrq_sched_debug_show();
 #endif
