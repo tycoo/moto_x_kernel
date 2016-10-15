432a7f227f670f18f383195477ebef69692527f4
tycoo <echotool@gmx.de>
2016-10-15 17:23:13 +0200

    Revert "printk: add cpu info into kernel log buffer"
    This reverts commit 275deb74d7ba6439899ab805cf8322cdced85d57.

--- kernel/printk.c
+++ kernel/printk.c
@@ -994,9 +994,9 @@ asmlinkage int vprintk(const char *fmt, va_list args)
 
 				t = cpu_clock(printk_cpu);
 				nanosec_rem = do_div(t, 1000000000);
-				tlen = sprintf(tbuf, "[%5lu.%06lu,%d] ",
+				tlen = sprintf(tbuf, "[%5lu.%06lu] ",
 						(unsigned long) t,
-						nanosec_rem / 1000, this_cpu);
+						nanosec_rem / 1000);
 
 				for (tp = tbuf; tp < tbuf + tlen; tp++)
 					emit_log_char(*tp);
